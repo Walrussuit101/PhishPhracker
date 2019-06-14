31d7c74972c3c8b99baba4ec5108eac53de22ded
Walrussuit101 <tjefferson@ycp.edu>
2019-06-14 14:30:13 -0400

    some kind of splash screen added

--- PhishPhracker/src/main/Main.java
+++ PhishPhracker/src/main/Main.java
@@ -1,7 +1,34 @@
 package main;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.*;
+
+import javax.swing.*;
+
 public class Main {
 	public static void main(String[] args) {
-        System.out.println("Hello World!");
+		
+		//Initialize the JFrame, set to maximized
+        JFrame frame = new JFrame("Phish Phracker");
+        	
+       
+        JLabel l = new JLabel(new ImageIcon("res/phish.jpg"));
+        frame.getContentPane().add(l, BorderLayout.CENTER);
+        frame.pack();
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        Dimension labelSize = l.getPreferredSize();
+        frame.setLocation(screenSize.width/2 - (labelSize.width/2), screenSize.height/2 - (labelSize.height/2));
+        frame.addMouseListener(new MouseAdapter()
+            {
+                public void mousePressed(MouseEvent e)
+                {
+                    frame.setVisible(false);
+                    frame.dispose();
+                }
+            });
+        frame.setVisible(true);
+        frame.setExtendedState(frame.getExtendedState() | JFrame.MAXIMIZED_BOTH);
     }
 }
