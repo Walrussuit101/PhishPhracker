548c09f833d963d48892ad91eb2235fe9c48f464
Walrussuit101 <tjefferson@ycp.edu>
2019-06-14 17:29:15 -0400

    refactored for screenBuilders

--- PhishPhracker/src/main/Main.java
+++ PhishPhracker/src/main/Main.java
@@ -3,18 +3,21 @@ package main;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.GridBagLayout;
-import java.awt.event.*;
-import java.util.concurrent.TimeUnit;
 
 import javax.swing.*;
 
 public class Main {
+	
+	/**Simply builds the initial frame/displays splash screen,
+	 * Then hands things off to other builders for other screens
+	 */
 	public static void main(String[] args) {
 		
-		//Initialize the JFrame, set to maximized
+		//Initialize the JFrame, set to maximized, end program on close
         JFrame frame = new JFrame("Phish Phracker");
         frame.setVisible(true);
         frame.setExtendedState(frame.getExtendedState() | JFrame.MAXIMIZED_BOTH);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         
         //Initialize splash screen/logo
         JPanel splash = new JPanel();
---
+++ PhishPhracker/src/screenBuilders/MainBuilder.java
@@ -0,0 +1,5 @@
+package screenBuilders;
+
+public class MainBuilder {
+
+}
