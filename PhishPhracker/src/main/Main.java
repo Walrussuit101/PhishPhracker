6298429a0e03ec6d7e07b17083360ae8fcf51cf7
Walrussuit101 <tjefferson@ycp.edu>
2019-06-14 17:20:23 -0400

    comments

--- PhishPhracker/src/main/Main.java
+++ PhishPhracker/src/main/Main.java
@@ -14,25 +14,27 @@ public class Main {
 		//Initialize the JFrame, set to maximized
         JFrame frame = new JFrame("Phish Phracker");
         frame.setVisible(true);
+        frame.setExtendedState(frame.getExtendedState() | JFrame.MAXIMIZED_BOTH);
         
+        //Initialize splash screen/logo
         JPanel splash = new JPanel();
         splash.setBackground(Color.BLACK);
         splash.setLayout(new GridBagLayout());
         splash.setVisible(true);
-        
         JLabel logo = new JLabel(new ImageIcon("res/phish.jpg"));
         splash.add(logo);
         
+        //add splash screen panel to frame
         frame.getContentPane().add(splash, BorderLayout.CENTER);
         
-        frame.setExtendedState(frame.getExtendedState() | JFrame.MAXIMIZED_BOTH);
-        
+        //wait three seconds before removing splash screen
         try{
-        	Thread.sleep(2000);
+        	Thread.sleep(3000);
         	logo.setVisible(false);
         	splash.setBackground(Color.WHITE);
         }catch(InterruptedException e) {
         	System.out.println(e);
         }   
+        
     }
 }
